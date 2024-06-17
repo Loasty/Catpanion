e458e9be0ead16fe6d4b3b1b25ed51e9fd073655
Loasty <tadyand147@gmail.com>
2024-06-17 02:48:11 -0400

    Progress save -- more stuff with settings menu

--- CatsMakeMeLaugh/Assets/Scripts/Enums.cs
+++ CatsMakeMeLaugh/Assets/Scripts/Enums.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 
 public class Enums : MonoBehaviour
 { 
+    
+    //NOVEL RELATED
     public enum DialogueSpeed { NORMAL, SLOW, VERYSLOW, FAST, VERYFAST };
-
-    public enum CatType { NONE, WHITE, TABBY, CALICO, BLACK };
-   
-    public enum Emotes { NONE, HEART, SIGH, ANNOYED  };
-
+    public enum Locations { NONE, FIELD, BARN };
+    public enum Emotes { NONE, HEART, SIGH, ANNOYED };
     public enum EmoteEffect { NONE, WIGGLE, RESIZE, GO_UP, GO_DOWN };
 
-    public enum Locations { NONE, FIELD, BARN };
 
-    public enum Actions { SIT = 30, WALK = 60, GREET = 75, SWIPE = 80, AFFECTION = 85, ATTENTION = 90, MEOW = 95, PLAY = 100 }
+    //CAT RELATED
+    public enum CatType { NONE, WHITE, TABBY, CALICO, BLACK };
+    public enum CatPersonality { Average, Confident, Energetic, Goofy, Gentle, Grumpy, Lazy, Timid, };
+    //Average - Normal behaviours, no special traits
+    //Confident - Likes to walk around more often, almost like strutting behaviour
+    //Energetic - Likes to play more often and sleep less
+    //Goofy - Likes to emote and meow more often, maybe gets zoomie behaviour often?
+    //Gentle - More likely to be Affectionate rather than Attention Seeking
+    //Grumpy - More likely to be Attention Seeking than Affectionate
+    //Lazy - Will sleep more than any other action
+    //Timid - Will hide & less affectionate at lower affection levels, but more present and affectionate as it rises
+    public enum CatMood { };
+    public enum CatGender { Boy, Girl};
+    public enum Actions { SIT = 30, WALK = 60, GREET = 75, SWIPE = 80, AFFECTION = 85, ATTENTION = 90, MEOW = 95, PLAY = 100 };
 
 
+    //MENU RELATED
+    public enum MenuOptions_Main { Main, Cats, Closet, Visit, Options };
+    public enum MenuOptions_Cats { Manage, Info, Adopt };
+    public enum MenuOptions_Closet { };
+    public enum MenuOptions_Visit { Store, Salon, Friend };
+    public enum MenuOptions_Options { Settings, Help, Exit };
 
-    public enum MenuOptions_Main { Main, Cats, Closet, Visit, Options }
-    public enum MenuOptions_Cats { Manage, Info, Adopt }
-    public enum MenuOptions_Closet { }
-    public enum MenuOptions_Visit { Store, Salon, Friend }
-    public enum MenuOptions_Options { Settings, Help, Exit }
+
+    //DESKTOP GAMEPLAY RELATED
+    public enum PlayAreaBorders { TOP, BOTTOM, LEFT, RIGHT };
 }
