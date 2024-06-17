be00d6685012b1fc8b1fbd2198fb9643f60fc0e9
Loasty <tadyand147@gmail.com>
2024-06-17 19:11:24 -0400

    Renamed CatsMakeMeLaugh to Catpanion_Unity

---
+++ Catpanion_Unity/Assets/SecureData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SecureData
+{
+    public static readonly string EncryptionKey = "Cotton-Eyed-Joe-BabyBoy";
+}
